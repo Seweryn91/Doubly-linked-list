ddc4dd40a8b26edfd0f0a908579a706a67af6bc6
Seweryn91 <s.talaj@gmail.com>
2019-01-23 13:14:24 +0100

    Implement testHead, testTail and testInsert methods

--- src/test/java/DoublyLinkedListTest.java
+++ src/test/java/DoublyLinkedListTest.java
@@ -5,14 +5,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DoublyLinkedListTest {
 
+    @Test
+    @DisplayName("Test head")
+    void testHead() {
+        DoublyLinkedList list = createList(5);
+        assertEquals("0", list.head().toString());
+    }
+
+    @Test
+    @DisplayName("Test tail")
+    void testTail() {
+        DoublyLinkedList list = createList(5);
+        assertEquals("4", list.tail().toString());
+    }
+
+    @Test
+    @DisplayName("Test insert")
+    void testInsert() {
+        DoublyLinkedList list = createList(5);
+        list.insert(1, "7");
+        assertEquals("0 7 1 2 3 4", list.toString());
+    }
 
     private DoublyLinkedList createList(int size){
         DoublyLinkedList list = new DoublyLinkedList();
-        for(int i=0; i < size; i++) {
+        for (int i=0; i < size; i++) {
             list.add(i);
         }
 
         return list;
     }
+
+
 
 }
